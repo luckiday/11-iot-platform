2b261a2ad90924f95307425c489b876ca5b61480
Sriapth Mishra <sripathm2@gmail.com>
2020-11-09 14:29:44 -0800

    Completed starter server.

--- src/Server/main.py
+++ src/Server/main.py
@@ -1,9 +1,13 @@
 import os
+import postgresql
+import datetime
+import postgresql.driver as pg_driver
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_jwt import JWT
 from flask import request
 
+last_message = ''
 def create_app():
 
     flask_app = Flask(__name__)
@@ -18,10 +22,12 @@ def create_app():
     @flask_app.route('/message',methods = ['POST', 'GET'])
     def message():
         if request.method == 'POST':
+            global last_message
             last_message = request.json['message']
-            return 'Post route!'
+            return 'Success!'
         else:
-            return 'Get route!'
+            global last_message
+            return last_message
 
     return flask_app
 
