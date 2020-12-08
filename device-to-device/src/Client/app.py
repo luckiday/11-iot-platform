760f2d9614b12d303e8f1fd91d1569ee6eea9f2d
kevinyao93 <kevinyao93@gmail.com>
2020-12-08 11:08:54 -0600

    update files

---
+++ device-to-device/src/Client/app.py
@@ -0,0 +1,88 @@
+import requests
+import argparse
+import sys
+import simplejson as json
+import base64
+import socket
+
+from flask import Flask, jsonify, abort, make_response, redirect, request, render_template
+
+app = Flask(__name__)
+
+serverIP = '192.168.10.20:8000'
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+currentIP = '{}:5000'.format(local_ip)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--deviceID', help='Please select a device from the list')
+parser.add_argument('--message', help='Please send a message to device')
+
+args = parser.parse_args()
+
+available_devices = []
+messages = []
+failed_messages = []
+
+def get_devices():
+    deviceRequest = requests.get('http://' + serverIP + '/api/devices')
+    return deviceRequest.json()
+
+def ping_server():
+    global available_devices
+    deviceRequest = requests.get('http://' + serverIP + '/api/devices/' + currentIP)
+    if 'device' in deviceRequest.json():
+        print('Device in Server', file=sys.stderr)
+    else:
+        device = {'ip' : currentIP}
+        deviceRequest = requests.post('http://' + serverIP + '/api/devices', json = device)
+    available_devices = get_devices()['devices']
+
+@app.route('/')
+def home():
+    ping_server()
+    return render_template('index.html', available_devices = available_devices, messages=messages)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    message = request.form.get('message')
+    option = request.form.get('devices')
+    json_option = json.loads(option.replace("'", "\""))
+    message_bytes = message.encode('ascii')
+    send_message(json_option['ip'], base64.b64encode(message_bytes))
+    return redirect('/')
+
+
+@app.route('/api/messages', methods=['GET'])
+def get_messages():
+    return jsonify({'messages': messages})
+
+@app.route('/api/messages', methods=['POST'])
+def create_message():
+    if not request.json:
+        abort(400)
+
+    decoded_message = base64.b64decode(request.json['message']).decode('ascii')
+    print(request.json, file=sys.stderr)
+    message = {
+        'origin_device': request.json['origin_device'],
+        'id': request.json['id'],
+        'message': decoded_message,
+        'done': False
+    }
+    messages.append(message)
+    return jsonify({'message': message}), 201
+
+def send_message(target_ip, message):
+    ping_server()
+    device = [device for device in available_devices if device['ip'] == target_ip]
+    if not device:
+        print('Target Device does not exist', file=sys.stderr)
+        return
+    json_message = {'target_device' : device[0], 'message': message}
+    messageRequest = requests.post('http://' + serverIP + '/api/messages', json = json_message)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ device-to-device/src/Server/app.py
@@ -0,0 +1,116 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, request
+import requests
+import logging
+import sys
+import sched, time
+
+app = Flask(__name__)
+# id, device, message, done
+messages = []
+message_scheduler = sched.scheduler(time.time, time.sleep)
+failed_messages = []
+sent_messages = []
+
+# ip
+devices = []
+currentID = 0
+
+def ping_device(message_index):
+    message = messages[message_index]
+    try:
+        response = requests.post('http://' + message['target_device']['ip'] + '/api/messages', json = message)
+    except:
+        print('Error', file=sys.stderr)
+        failed_messages.append(messages.pop(message_index))
+    else:
+        sent_messages.append(messages.pop(message_index))
+    print(sent_messages, file=sys.stderr)
+
+
+
+def send_messages():
+    for idx, message in enumerate(messages):
+        ping_device(idx)
+
+
+@app.route('/api/devices', methods=['GET'])
+def get_devices():
+    return jsonify({'devices': devices})
+
+@app.route('/api/devices/<device_ip>', methods=['GET'])
+def get_device(device_ip):
+    device = [device for device in devices if device['ip'] == device_ip]
+    if len(device) == 0:
+        abort(404)
+    return jsonify({'device': device[0]})
+
+@app.route('/api/devices', methods=['POST'])
+def create_device():
+    if not request.json or not 'ip' in request.json:
+        abort(400)
+    device = {
+        'ip': request.json['ip']
+    }
+    devices.append(device)
+    return jsonify({'device': device}), 201
+
+@app.route('/api/messages', methods=['GET'])
+def get_messages():
+    return jsonify({'messages': messages})
+
+@app.route('/api/messages/<int:message_id>', methods=['GET'])
+def get_message(message_id):
+    message = [message for message in messages if message['id'] == message_id]
+    if len(message) == 0:
+        abort(404)
+    return jsonify({'message': message[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/api/messages', methods=['POST'])
+def create_message():
+    global currentID
+    if not request.json or not 'target_device' in request.json:
+        abort(400)
+    currentID += 1
+    message = {
+        'id': currentID,
+        'origin_device': request.remote_addr,
+        'target_device': request.json['target_device'],
+        'message': request.json['message'],
+        'repeated_sends': 0
+    }
+    messages.append(message)
+    send_messages()
+    return jsonify({'message': message}), 201
+
+@app.route('/api/messages/<int:message_id>', methods=['PUT'])
+def update_message(message_id):
+    message = [message for message in messages if message['id'] == message_id]
+    if len(message) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'message' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    message[0]['message'] = request.json.get('message', message[0]['message'])
+    message[0]['done'] = request.json.get('done', message[0]['done'])
+    return jsonify({'message': message[0]})
+
+@app.route('/api/messages/<int:message_id>', methods=['DELETE'])
+def delete_message(message_id):
+    message = [message for message in messages if message['id'] == message_id]
+    if len(message) == 0:
+        abort(404)
+    messages.remove(message[0])
+    return jsonify({'result': True})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
